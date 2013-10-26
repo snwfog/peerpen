03549cf7db65d1b90398a550f63dea61d938a5f0
Harry Zhang <haiyuezhang@hotmail.com>
2013-10-26 19:19:58 -0400

    edit profile

--- src/main/java/com/peerpen/controller/Profile.java
+++ src/main/java/com/peerpen/controller/Profile.java
@@ -3,10 +3,7 @@ package com.peerpen.controller;
 import com.peerpen.model.Peer;
 import com.sunnyd.database.Manager;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,39 +19,34 @@ import java.util.HashMap;
  * Time: 7:05 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Register extends HttpServlet {
+public class Profile extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //doGet(request, response);
-        // get all register input
+        // get all profile input
+        int id = Integer.parseInt(request.getParameter("id"));
         String first_name = request.getParameter("first_name");
         String last_name = request.getParameter("last_name");
         String website = request.getParameter("personal_website");
-        String user_name = request.getParameter("user_name");
         String email = request.getParameter("email");
-        String password = request.getParameter("password");
+
 
         // here we should validate the input...
 
         // check if user already exists
         HashMap<String, Object> map = new HashMap<String, Object>();
-        map.put("userName", user_name);
+        map.put("firstName", first_name);
+        map.put("lastName", last_name);
+        map.put("personalWebsite", website);
+        map.put("email", email);
+
+
         String redirect = "/error";
         try {
-            ArrayList<HashMap<String, Object>> matches = Manager.findAll("peers", map);
-            if (matches.size() == 0){ // means user does not exist
-                Peer newPeer = new Peer();
-                newPeer.setFirstName(first_name);
-                newPeer.setLastName(last_name);
-                newPeer.setUserName(user_name);
-                newPeer.setEmail(email);
-                newPeer.setPersonalWebsite(website);
-                newPeer.setPassword(password);
-                newPeer.save();
 
-                HttpSession session = request.getSession();
-                session.setAttribute("user", user_name);
-                redirect = "/profile";
-            }
+            System.out.println("true or fals?" + Manager.update(id,"peers",map));
+            redirect = "/profile";
+            Peer peer = new Peer(Manager.find(id, "peers"));
+            HttpSession session = request.getSession();
+            session.setAttribute("user", peer);
             response.sendRedirect(redirect);
 
         } catch (Exception e) {
