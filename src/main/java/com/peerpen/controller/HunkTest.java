223b9c5371ea2a5c4f43facc07439b88b2f9b85b
Harry Zhang <haiyuezhang@hotmail.com>
2013-11-08 21:38:59 -0500

    reapply the change

--- src/main/java/com/peerpen/controller/HunkTest.java
+++ src/main/java/com/peerpen/controller/HunkTest.java
@@ -25,15 +25,6 @@ public class HunkTest extends HttpServlet
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
   {
     String raw = request.getParameter("json_hunks");
-    //System.out.println("raw" + raw);
-
-//        Hunk a = new Hunk();
-//        a.setIdView("box3");
-//        a.setContent("div");
-//        Document d = new Document();
-//        a.setDocument(d);
-//        System.out.println(a.getContent().toString());
-
     JsonParser parser = new JsonParser();
     JsonObject rootObj = parser.parse(raw).getAsJsonObject();
 
@@ -45,33 +36,28 @@ public class HunkTest extends HttpServlet
     Iterator<JsonElement> modifiedIterator = modifiedList.iterator();
     Iterator<JsonElement> deletedIterator = deletedList.iterator();
 
-    // CREATE
-//    ArrayList<Hunk> createdHunkList = new ArrayList<Hunk>();
-//    Document newDoc = new Document();
-//    newDoc.setDocName("newDoc");
-//    newDoc.setPeerId(1);
-//    newDoc.save();
-//    while (createdIterator.hasNext())
-//    {
-//      JsonObject ob = createdIterator.next().getAsJsonObject().getAsJsonObject();
-//      String boxid = ob.get("id").toString();
-//      String html = ob.get("html").toString();
-//            Hunk h = new Hunk();
-//            h.setDocumentId(newDoc.getId());
-//            h.setIdView(boxid);
-//            h.setContent(html);
-//            createdHunkList.add(h);
-//    }
-//
-//    for (Hunk h : createdHunkList)
-//    {
-//      h.save();
-//    }
+    // CREATE (this will insert a new hunk into db)
+    Document newDoc = new Document();
+    newDoc.setDocName("newDoc");
+    newDoc.setPeerId(1);
+    newDoc.save();
+    while (createdIterator.hasNext())
+    {
+      JsonObject ob = createdIterator.next().getAsJsonObject().getAsJsonObject();
+      String receivedIdView = ob.get("id").toString();
+      String receivedHtml = ob.get("html").toString();
+      Hunk hunk = new Hunk();
+      hunk.setDocumentId(newDoc.getId());
+      hunk.setIdView(receivedIdView);
+      hunk.setContent(receivedHtml);
+      hunk.save();
+    }
 
 
-    // MODIFY
+
+    // MODIFY (this will update the hunk if owner, or insert a changeset if peer)
     // check if session user is the document owner?
-    Boolean isOwner = false;
+    Boolean isOwner = true;
     while (modifiedIterator.hasNext())
     {
       // received: idView, newhtml content
@@ -90,13 +76,13 @@ public class HunkTest extends HttpServlet
 
       if (isOwner)
       {
-        // update the hunk content and save
+        // update the old hunk directly and save
         existingHunk.setContent(receivedHtml);
         existingHunk.update();
       }
       else
       {
-        // get the uid based on idView given
+        // create a new changeset
         HashMap<String, Object> changesetData = new HashMap<String, Object>();
         changesetData.put("hunkId", existingHunkId);
         changesetData.put("content", receivedHtml);
@@ -124,15 +110,6 @@ public class HunkTest extends HttpServlet
       existingHunk.Destroy();
 
     }
-
-//      Hunk h = new Hunk();
-//
-//      h.setDocumentId(newDoc.getId());
-//      h.setIdView(boxid);
-//      h.setContent(html);
-//      list.add(h);
-
-
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
