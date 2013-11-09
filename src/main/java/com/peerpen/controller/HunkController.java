266c47396f621453a2377e385bd20b1fd70e73a2
snwfog <donchoa@gmail.com>
2013-11-08 21:45:51 -0500

    Update pom with dependency

---
+++ src/main/java/com/peerpen/controller/HunkController.java
@@ -0,0 +1,12 @@
+package com.peerpen.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: snw
+ * Date: 11/8/2013
+ * Time: 8:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HunkController
+{
+}
