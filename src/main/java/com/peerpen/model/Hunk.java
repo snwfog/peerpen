cd4e9016959bf9887bf99dbac91771e356391f5d
snwfog <donchoa@gmail.com>
2013-11-15 12:41:14 -0500

    Merge with dev

--- src/main/java/com/peerpen/model/Hunk.java
+++ src/main/java/com/peerpen/model/Hunk.java
@@ -2,8 +2,9 @@ package com.peerpen.model;
 
 import com.sunnyd.Base;
 import com.sunnyd.IModel;
-import com.sunnyd.annotations.*;
-import com.sunnyd.database.Manager;
+import com.sunnyd.annotations.ActiveRecordField;
+import com.sunnyd.annotations.ActiveRelationHasMany;
+import com.sunnyd.annotations.ActiveRelationHasOne;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,20 +17,16 @@ import java.util.HashMap;
  * To change this template use File | Settings | File Templates.
  */
 public class Hunk extends Base implements IModel {
-    public static final String tableName = "hunks";
 
+    public static final String tableName = "hunks";
     @ActiveRecordField
     private String idView;
-
     @ActiveRecordField
     private String content;
-
     @ActiveRecordField
     private Integer documentId;
-
     @ActiveRelationHasOne
     private Document document;
-
     @ActiveRelationHasMany
     private ArrayList<Changeset> changesets;
 
@@ -37,50 +34,61 @@ public class Hunk extends Base implements IModel {
         super();
     }
 
-    public Hunk(HashMap<String, Object> HM) {
-        super(HM);
+    public Hunk( HashMap<String, Object> HM ) {
+        super( HM );
+    }
+
+    public static void main( String[] args ) {
+        Hunk h = new Hunk();
+        h.setContent( "ababaabbbabbababb" );
+        h.setDocumentId( 4 );
+        System.out.println( h.save() );
+
+        Hunk a = new Hunk().find( h.getId() );
+        System.out.println( "lplplplp" + a.getContent() );
+        System.out.println( a.getDocument().getDocName() );
+
     }
 
     public String getIdView() {
         return idView;
     }
 
-    public void setIdView(String idView) {
+    public void setIdView( String idView ) {
         this.idView = idView;
-        setUpdateFlag(true);
+        setUpdateFlag( true );
     }
 
     public String getContent() {
         return content;
     }
 
-    public void setContent(String content) {
+    public void setContent( String content ) {
         this.content = content;
-        setUpdateFlag(true);
+        setUpdateFlag( true );
     }
 
-    public void setDocumentId(Integer documentId){
+    public Integer getDocumentId() {
+        return this.documentId;
+    }
+
+    public void setDocumentId( Integer documentId ) {
         this.documentId = documentId;
     }
 
-    public Integer getDocumentId(){
-          return this.documentId;
-    }
-
-    public void setDocument(Document document){
-        this.documentId = document.getId();
-        this.document = null;
-        setUpdateFlag(true);
-    }
-
-    public Document getDocument(){
-        initRelation("document");
+    public Document getDocument() {
+        initRelation( "document" );
         return document;
     }
 
-    public ArrayList<Changeset> getChangesets()
-    {
-        initRelation("changesets");
+    public void setDocument( Document document ) {
+        this.documentId = document.getId();
+        this.document = null;
+        setUpdateFlag( true );
+    }
+
+    public ArrayList<Changeset> getChangesets() {
+        initRelation( "changesets" );
         return this.changesets;
     }
     public static void main(String[] args) {
@@ -94,7 +102,4 @@ public class Hunk extends Base implements IModel {
 //        System.out.println(a.getDocument().getDocName());
 
     }
-
-
-
 }
