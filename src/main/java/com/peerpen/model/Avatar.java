136106811187ce9ed3167f68582d63963173aca9
Wais Khedri <khedriwais@hotmail.com>
2013-11-15 23:12:57 -0500

    added avatar model

---
+++ src/main/java/com/peerpen/model/Avatar.java
@@ -0,0 +1,72 @@
+package com.peerpen.model;
+
+import com.sunnyd.Base;
+import com.sunnyd.IModel;
+import com.sunnyd.annotations.ActiveRecordField;
+import com.sunnyd.annotations.ActiveRelationHasOne;
+
+import java.sql.Blob;
+import java.util.HashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Wais
+ * Date: 11/15/13
+ * Time: 11:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Avatar extends Base implements IModel {
+
+    public static final String tableName = "avatars";
+
+    @ActiveRecordField
+    private String imgTitle;
+
+    @ActiveRecordField
+    private Blob imgData;
+
+    @ActiveRelationHasOne
+    private Peer peer;
+
+    @ActiveRecordField
+    private Integer peerId;
+
+
+    public Avatar() {
+        super();
+    }
+
+    public Avatar(HashMap<String, Object> HM) {
+        super(HM);
+    }
+
+
+    public String getImgTitle() {
+        return imgTitle;
+    }
+
+    public void setImgTitle(String imgTitle) {
+        this.imgTitle = imgTitle;
+    }
+
+    public Blob getImgData() {
+        return imgData;
+    }
+
+    public void setImgData(Blob imgData) {
+        this.imgData = imgData;
+    }
+
+    public Peer getPeer(){
+        initRelation("peer");
+        return peer;
+    }
+
+    public Integer getPeerId() {
+        return peerId;
+    }
+
+    public void setPeerId(Integer peerId) {
+        this.peerId = peerId;
+    }
+}
