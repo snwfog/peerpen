529ee4713c313bcaff42a1fc631aff040e55173a
Harry Zhang <haiyuezhang@hotmail.com>
2014-01-09 16:03:33 -0500

    add base files for search

---
+++ src/main/java/com/peerpen/controller/SearchController.java
@@ -0,0 +1,27 @@
+package com.peerpen.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: momoking
+ * Date: 1/9/2014
+ * Time: 3:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SearchController extends HttpServlet {
+
+    protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+    }
+
+    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+    }
+}
