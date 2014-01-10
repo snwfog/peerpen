c285b9c8b42ed4d184e3028b0155601592b7dc71
Harry Zhang <haiyuezhang@hotmail.com>
2014-01-09 20:11:34 -0500

    link basic search view to controller, plus basic logic in controller not finished.

--- src/main/java/com/peerpen/controller/SearchController.java
+++ src/main/java/com/peerpen/controller/SearchController.java
@@ -1,12 +1,17 @@
 package com.peerpen.controller;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import com.sunnyd.models.*;
+import com.sunnyd.database.Manager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,10 +23,28 @@ import javax.servlet.http.HttpServletResponse;
 public class SearchController extends HttpServlet {
 
     protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-
+        doGet( request, response );
     }
 
     protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        String query = request.getParameter( "query" );
+        String area = request.getParameter( "area" ); // area of search
+        String origin = request.getRequestURI();
+
+        System.out.println(query + " " + origin + " " + area);
+
+        // Build Search Conditions
+        Map<String, Object> conditions = new HashMap<String, Object>(  );
+        conditions.put("firstName", "test");
+
+        // Find
+        ArrayList results = Manager.findAll( "peers", conditions );
+        System.out.println(results.toString());
+
+
+
+
+
 
     }
 }
--- src/main/webapp/view/search.jsp
+++ src/main/webapp/view/search.jsp
@@ -1,1 +1,16 @@
-search view
+<%@ include file="/view/includes/static/header.jsp" %>
+
+<form action="search.do" method="get" align="center">
+    Search <input type="text" name="query" />
+    <input type="submit" name="submit" value="OK" />
+    <br />
+    <input type="radio" name="area" value="all" checked />All
+    <input type="radio" name="area" value="documents" />Documents
+    <input type="radio" name="area" value="peers" />Peers
+    <input type="radio" name="area" value="groups" />Groups
+    <input type="radio" name="area" value="tags" />Tags
+</form>
+
+
+
+<%@ include file="/view/includes/static/footer.jsp" %>
