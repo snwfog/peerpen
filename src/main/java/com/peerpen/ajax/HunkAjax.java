0fa2a90bdbf9b1d8459117d6237f4d5ddc078782
snwfog <donchoa@gmail.com>
2014-01-13 13:03:36 -0500

    Merge with remote

---
+++ src/main/java/com/peerpen/ajax/HunkAjax.java
@@ -0,0 +1,17 @@
+package com.peerpen.ajax;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by snw on 1/11/2014.
+ */
+public class HunkAjax extends HttpServlet {
+
+    protected void doPost( HttpServletRequest request, HttpServletResponse response ) {
+
+
+
+    }
+}
