2aa0c2c88c13192b01886cb53e10f0b00c250193
Bobby Yit <bobby.yit@gmail.com>
2014-01-24 13:17:10 -0500

    -fix comments

--- src/main/java/com/peerpen/controller/DocumentController.java
+++ src/main/java/com/peerpen/controller/DocumentController.java
@@ -24,18 +24,12 @@ public class DocumentController extends HttpServlet
 
     Map<String, String> parameters = (Map<String, String>) request.getAttribute("parameters");
     System.out.println("Request parameters:" + parameters);
-    HttpSession session = request.getSession();
-//        Peer peer = (Peer) session.getAttribute( "user" );
-//    Temp fix.. session above returns null
     Peer peer = new Peer().find(Integer.parseInt(parameters.get("peer")));
-    session.setAttribute("user", peer);
-
     Document document = new Document().find(Integer.parseInt(parameters.get("document.do")));
 
     List<Comment> comments = document.getOrderedComments();
-//    List<Changeset> changesets = document.getChangesets();
 
-
+    request.setAttribute("user", peer);
     request.setAttribute("comments", comments);
     request.setAttribute("document", document);
     request.getRequestDispatcher("/document").forward(request, response);
@@ -44,38 +38,38 @@ public class DocumentController extends HttpServlet
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException
   {
-    if (request.getParameter("_method") != null)
+    Map<String, String> parameters = (Map<String, String>) request.getAttribute("parameters");
+    if (parameters.get("method") != null)
     {
-      if (request.getParameter("_method").contentEquals("_delete"))
+      if (parameters.get("method").contentEquals("_delete"))
       {
         doDelete(request, response);
       }
-      else if (request.getParameter("_method").contentEquals("_doPut"))
+      else if (parameters.get("method").contentEquals("_doPut"))
       {
         doPut(request, response);
       }
     }
     else
     {
-      HttpSession session = request.getSession();
-      Peer peer = (Peer) session.getAttribute("user");
-      Document document = new Document().find(Integer.parseInt(request.getParameter("docId")));
 
+      Peer peer = new Peer().find(Integer.parseInt(parameters.get("peerid")));
+      Document document = new Document().find(Integer.parseInt(parameters.get("docid")));
 
       Comment comment = new Comment();
-      comment.setMessage(request.getParameter("comment").toString());
+      comment.setMessage(parameters.get("comment"));
       comment.setName(peer.getFirstName() + " " + peer.getLastName());
       comment.setPeerId(peer.getId());
       comment.setDocumentId(document.getId());
       comment.setUpVote(0);
       comment.setDownVote(0);
-
       comment.save();
 
       List<Comment> comments = document.getOrderedComments();
 
       request.setAttribute("comments", comments);
       request.setAttribute("document", document);
+      request.setAttribute("user", peer);
       request.getRequestDispatcher("/document").forward(request, response);
     }
   }
@@ -86,18 +80,20 @@ public class DocumentController extends HttpServlet
   protected void doDelete(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException
   {
-    HttpSession session = request.getSession();
-    Peer peer = (Peer) session.getAttribute("user");
-    Document document = new Document().find(Integer.parseInt(request.getParameter("docId")));
+    Map<String, String> parameters = (Map<String, String>) request.getAttribute("parameters");
 
-    Comment comment = new Comment().find(Integer.parseInt(request.getParameter("commentId")));
+    Peer peer = new Peer().find(Integer.parseInt(parameters.get("peerid")));
+    Document document = new Document().find(Integer.parseInt(request.getParameter("docid")));
+
+    Comment comment = new Comment().find(Integer.parseInt(request.getParameter("commentid")));
     comment.destroy();
 
     List<Comment> comments = document.getOrderedComments();
 
     request.setAttribute("comments", comments);
     request.setAttribute("document", document);
-    request.getRequestDispatcher("/document").forward(request, response);
+    request.setAttribute("user", peer);
+    response.sendRedirect("peer/2/document.do/1");
   }
 
   protected void doPut(HttpServletRequest request, HttpServletResponse response)
