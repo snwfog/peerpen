5af92ddaa6965ee3bfdd525f8f85cf3de9398b95
Ken Tran <k.tran850@gmail.com>
2014-02-13 19:46:38 -0500

    Putting back some JS for profile

--- src/main/java/com/peerpen/controller/ProfileController.java
+++ src/main/java/com/peerpen/controller/ProfileController.java
@@ -1,6 +1,9 @@
 package com.peerpen.controller;
 
+import com.google.common.collect.ImmutableMap;
 import com.peerpen.model.Peer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,65 +24,71 @@ import java.text.DateFormat;
  * To change this template use File | Settings | File Templates.
  */
 public class ProfileController extends HttpServlet {
+//    static final Logger logger = LoggerFactory.getLogger(ProfileController.class);
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // get all profile input
-
-        System.out.println("MIKIIIKEIKEIKaiosdoaijdasoij");
-        int id = Integer.parseInt(request.getParameter("id"));
-        String first_name = request.getParameter("first_name");
-        String last_name = request.getParameter("last_name");
-        String website = request.getParameter("personal_website");
-        String email = request.getParameter("email");
-        String description = request.getParameter("description");
-        String country = request.getParameter("country");
-        String gender = request.getParameter("gender");
-        String industry = request.getParameter("industry");
-        int yoe = Integer.parseInt(request.getParameter("yoe"));
-        String dateOfBirth = (request.getParameter("dob"));
-        System.out.println("THIS IS IT!"+dateOfBirth);
-
-        Date dob = new Date();
-
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            dob = formatter.parse(dateOfBirth);
-            dateOfBirth = formatter.format(dob);
-        }catch (Exception e) {
-            System.out.println("Unable to parse date stamp");
-        }
-
-        // here we should validate the input...
-
-        // check if email already exists
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("email", email);
-
-        String redirect = "/error";
-        try {
-
-            redirect = "/profile";
-            Peer peer = new Peer().find(id);
-            peer.setFirstName(first_name);
-            peer.setLastName(last_name);
-            peer.setPersonalWebsite(website);
-            peer.setEmail(email);
-            peer.setDescription(description);
-            peer.setCountry(country);
-            peer.setEmail(email);
-            peer.setGender(gender);
-            peer.setIndustry(industry);
-            peer.setExperience(yoe);
-            peer.setDateOfBirth(dob);
-            peer.update();
-
-            HttpSession session = request.getSession();
-            session.setAttribute("user", peer);
-            session.setAttribute("birth_date", dateOfBirth);
-            response.sendRedirect(redirect);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Map<String, Object> parameters = (Map<String, Object>) request.getAttribute("parameters");
+        System.out.println(parameters);
+        Peer peer = new Peer(parameters);
+        peer.update();
+        response.sendRedirect("/peer/" + peer.getId() + "/profile");
+//
+//        System.out.println("MIKIIIKEIKEIKaiosdoaijdasoij");
+//        int id = Integer.parseInt(request.getParameter("id"));
+//        String first_name = request.getParameter("first_name");
+//        String last_name = request.getParameter("last_name");
+//        String website = request.getParameter("personal_website");
+//        String email = request.getParameter("email");
+//        String description = request.getParameter("description");
+//        String country = request.getParameter("country");
+//        String gender = request.getParameter("gender");
+//        String industry = request.getParameter("industry");
+//        int yoe = Integer.parseInt(request.getParameter("yoe"));
+//        String dateOfBirth = (request.getParameter("dob"));
+//        System.out.println("THIS IS IT!"+dateOfBirth);
+//
+//        Date dob = new Date();
+//
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+//        try {
+//            dob = formatter.parse(dateOfBirth);
+//            dateOfBirth = formatter.format(dob);
+//        }catch (Exception e) {
+//            System.out.println("Unable to parse date stamp");
+//        }
+//
+//        // here we should validate the input...
+//
+//        // check if email already exists
+//        Map<String, Object> map = new HashMap<String, Object>();
+//        map.put("email", email);
+//
+//        String redirect = "/error";
+//        try {
+//
+//            redirect = "/profile";
+//            Peer peer = new Peer().find(id);
+//            peer.setFirstName(first_name);
+//            peer.setLastName(last_name);
+//            peer.setPersonalWebsite(website);
+//            peer.setEmail(email);
+//            peer.setDescription(description);
+//            peer.setCountry(country);
+//            peer.setEmail(email);
+//            peer.setGender(gender);
+//            peer.setIndustry(industry);
+//            peer.setExperience(yoe);
+//            peer.setDateOfBirth(dob);
+//            peer.update();
+//
+//            HttpSession session = request.getSession();
+//            session.setAttribute("user", peer);
+//            session.setAttribute("birth_date", dateOfBirth);
+//            response.sendRedirect(redirect);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
 
     }
--- src/main/webapp/view/includes/static/header.jsp
+++ src/main/webapp/view/includes/static/header.jsp
@@ -33,6 +33,7 @@
     <link rel="stylesheet" href="/assets/css/custom/avatar.css" type="text/css">
     <link rel="stylesheet" href="/assets/css/custom/peerpen.css" type="text/css">
 
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css">
     <link rel="stylesheet" title="Standard" href="/assets/css/js/lib/contentflow/contentflow_src.css" type="text/css" />
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
