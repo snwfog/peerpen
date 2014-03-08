c5b9eacd2d930f42540a5b0b4c45714a4972a431
nimonikMike <viet_mike.pham@hotmail.com>
2014-03-08 18:55:25 -0500

    feedable test

--- src/test/java/com/peerpen/model/FeedableTest.java
+++ src/test/java/com/peerpen/model/FeedableTest.java
@@ -10,8 +10,23 @@ public class FeedableTest {
     @Test
     public void commentCreateFeedableTest()
     {
+        Integer oldHighestID = (Integer) Manager.find("SELECT max(id) as id FROM feedables").get("id");
+        Comment c = new Comment();
+        c.setMessage("Unit Testing");
+        c.setObjectId(1);
+        c.setType("Document");
+        c.save();
+        c.setPosterPeerId(1);
         Integer currentHighestID = (Integer) Manager.find("SELECT max(id) as id FROM feedables").get("id");
-        Comment c = new Comment("")
+        Integer ExpectedId = oldHighestID+1;
+        Assert.assertEquals(currentHighestID, ExpectedId);
+
+        Feedable f = new Feedable().find(currentHighestID);
+        Assert.assertNotNull(f);
+        Assert.assertEquals(f.getType(), "Comment");
+        Assert.assertEquals(f.getChildId(), c.getId());
+        Assert.assertTrue(c.destroy());
+        Assert.assertTrue(f.destroy());
     }
 
     @Test
