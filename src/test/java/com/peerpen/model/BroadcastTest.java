aa393cc2e6c39a029b8499e027aea551d4914a10
nimonikMike <viet_mike.pham@hotmail.com>
2014-03-14 10:53:26 -0400

    tests to do

---
+++ src/test/java/com/peerpen/model/BroadcastTest.java
@@ -0,0 +1,11 @@
+package com.peerpen.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: WAIS
+ * Date: 08/03/14
+ * Time: 10:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BroadcastTest {
+}
---
+++ src/test/java/com/peerpen/model/JoingroupTest.java
@@ -0,0 +1,11 @@
+package com.peerpen.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: MP
+ * Date: 08/03/14
+ * Time: 10:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JoingroupTest {
+}
